6d2d4fc86f5076006e564bf102633afe77841d35
arvinzojaji <122469880+arvinzojaji@users.noreply.github.com>
2023-05-31 16:22:48 -0400

    Initial commit

---
+++ .storybook/config.js
@@ -0,0 +1,42 @@
+import {
+  configure,
+  addParameters
+} from '@storybook/vue';
+
+import {
+  action
+} from '@storybook/addon-actions';
+
+const viewportOptions = {
+  defaultViewport: "iphone6"
+}
+
+addParameters({
+  viewport: viewportOptions
+});
+
+import Vue from 'vue';
+// import Vuex from 'vuex'; // Vue plugins
+// Install Vue plugins.
+// Vue.use(Vuex);
+
+// router-link missing component workaround
+Vue.component('RouterLink', {
+  props: ['to'],
+  methods: {
+    log() {
+      action('link target')(this.to)
+    },
+  },
+  template: '<a @click="log()"><slot>RouterLink</slot></a>',
+})
+import 'buefy/dist/buefy.css'
+import Buefy from 'buefy'
+Vue.use(Buefy)
+
+function loadStories() {
+  // You can require as many stories as you need.
+  require('../stories/index.stories');
+}
+
+configure(loadStories, module);
---
+++ components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image">
+        <img :src="image" alt="Image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img :src="thumb" alt="Image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">{{author}}</p>
+          <p class="subtitle is-6">{{user}}</p>
+        </div>
+      </div>
+
+      <div class="content">
+        {{text}}
+        <br>
+        <small>{{date}}</small>
+      </div>
+    </div>
+
+    <b-loading :active.sync="loading" :can-cancel="false"></b-loading>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: Boolean,
+    title: String,
+    text: String,
+    date: Date,
+    image: String,
+    author: String,
+    user: String,
+    thumb: String
+  }
+};
+</script>
+
+<style scoped>
+</style>
